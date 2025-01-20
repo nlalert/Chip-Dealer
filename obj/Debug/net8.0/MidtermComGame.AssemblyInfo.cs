52c66512212526e813df2280b7eb85f23b25fd83
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2025-01-21 02:37:24 +0700

    test git ignore

--- obj/Debug/net8.0/MidtermComGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MidtermComGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MidtermComGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4618e045a448c448f97125ebc93cc9aed6c6319c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a7efe757ac8c03f89edc0269ba7665ae96e71a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MidtermComGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MidtermComGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
