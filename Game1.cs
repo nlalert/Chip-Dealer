39aa1ebdb3a981885a20a8e049f82b1c46026fe0
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2025-01-21 02:28:46 +0700

    copy gameobject code from space invader

--- Game1.cs
+++ Game1.cs
@@ -2,8 +2,6 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace MidtermComGame;
-
 public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
