cd788c42aea6ce7c21136d2856b4c1f056d0f217
Feen <kakfeenza@gmail.com>
2025-01-21 18:41:52 +0700

    Angle Dection

--- GameObjects/Bubble.cs
+++ GameObjects/Bubble.cs
@@ -9,6 +9,7 @@ class Bubble : GameObject
     public float Angle;
     public float Speed;
 
+    private List<(Vector2, Vector2)> touchingPairs = new List<(Vector2, Vector2)>(); //for dev 
     // public float DistantMoved;
     public Bubble(Texture2D texture) : base(texture)
     {
@@ -19,6 +20,23 @@ class Bubble : GameObject
     {
         spriteBatch.Draw(_texture, Position, Viewport, Color.White);
         base.Draw(spriteBatch);
+        Texture2D lineTexture = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
+        lineTexture.SetData(new[] { Color.White });
+
+        foreach (var pair in touchingPairs)
+        {
+            DrawLine(spriteBatch, lineTexture, pair.Item1, pair.Item2, Color.Red, 2);
+        }
+    }
+    private void DrawLine(SpriteBatch spriteBatch, Texture2D texture, Vector2 start, Vector2 end, Color color, float thickness)
+    {
+        //for dev purpose
+        Vector2 direction = end - start;
+        float length = direction.Length();
+        float rotation = (float)Math.Atan2(direction.Y, direction.X);
+
+        spriteBatch.Draw(texture, start, null, color, rotation, Vector2.Zero, new Vector2(length, thickness), SpriteEffects.None, 0);
+      
     }
 
     public override void Reset()
@@ -29,27 +47,51 @@ class Bubble : GameObject
 
     public override void Update(GameTime gameTime, List<GameObject> gameObjects)
     {
-        Velocity.X = (float) Math.Cos(Angle) * Speed;
-        Velocity.Y = (float) Math.Sin(Angle) * Speed;
+        Velocity.X = (float)Math.Cos(Angle) * Speed;
+        Velocity.Y = (float)Math.Sin(Angle) * Speed;
 
         Position += Velocity * (float)(gameTime.ElapsedGameTime.TotalSeconds);
 
-        if (Position.Y < Singleton.PlayAreaEndY) 
-            Position.Y = Singleton.PlayAreaEndY;
-
-        if (Position.X < Singleton.PlayAreaStartX || Position.X > Singleton.PlayAreaEndX) 
-            Angle = (float)Math.PI - Angle;
-
-        foreach (GameObject s in gameObjects)
+        if (Position.Y < Singleton.PlayAreaEndY)
         {
-            if (IsTouching(s))
-            {
-                // Handle collision logic here (if required)
-            }
+            Position.Y = Singleton.PlayAreaEndY;
+            Speed = 0;
         }
 
-        Velocity = Vector2.Zero;
+        if (Position.X < Singleton.PlayAreaStartX || Position.X > Singleton.PlayAreaEndX)
+            Angle = (float)Math.PI - Angle;
 
-    base.Update(gameTime, gameObjects);
+        // foreach (GameObject s in gameObjects)
+        // {
+        //     if (IsTouching(s) && s is Bubble)
+        //     {
+                
+        //         // Add the touching pair to the list for dev
+        //         touchingPairs.Add((this.Position + new Vector2(_texture.Width/2,_texture.Height/2 ), s.Position+ new Vector2(_texture.Width/2,_texture.Height/2)));
+        //         Vector2 direction = s.Position - this.Position;
+        //         float collisionAngle = (float)Math.Atan2(direction.Y, direction.X);
+        //         Console.WriteLine($"Touched at angle: {MathHelper.ToDegrees(collisionAngle):F2} degrees");
+
+        //         Speed = 0;
+        //         break;
+        //     }
+        // }
+        for(int i = gameObjects.Count - 1; i >= 0; i--){
+            if(IsTouching(gameObjects[i]) && gameObjects[i] is Bubble){
+                //draw line for dev
+                touchingPairs.Add((this.Position + new Vector2(_texture.Width/2,_texture.Height/2 ), gameObjects[i].Position+ new Vector2(_texture.Width/2,_texture.Height/2)));
+                Vector2 direction = gameObjects[i].Position - this.Position;
+                float collisionAngle = (float)Math.Atan2(direction.Y, direction.X);
+                if(Speed !=0){
+                    Console.WriteLine($"Touched at angle: {MathHelper.ToDegrees(collisionAngle):F2} degrees");
+                }
+                Speed = 0;
+                break;
+            }
+        }
+        
+        
+        base.Update(gameTime, gameObjects);
+
     }
 }
