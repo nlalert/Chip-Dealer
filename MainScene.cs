f673a91f3de31da112f83123f722258f9f85ea43
Feen <kakfeenza@gmail.com>
2025-01-22 21:44:41 +0700

    test game menu

--- MainScene.cs
+++ MainScene.cs
@@ -1,137 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace MidtermComGame;
 
-public class MainScene : Game
+
+public class MainScene
 {
-    private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private Texture2D _bubbleTexture, _rectTexture;
+    private SpriteFont _font;
+    private List<GameObject> _gameObjects;
 
-    SpriteFont _font;
-
-    List<GameObject> _gameObjects;
-    int _numObject;
-
-    Texture2D _bubbleTexture;
-    Texture2D _rectTexture;
-
-    public MainScene()
-    {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
-    }
-
-    protected override void Initialize()
-    {
-        _graphics.PreferredBackBufferWidth = Singleton.SCREEN_WIDTH;
-        _graphics.PreferredBackBufferHeight = Singleton.SCREEN_HEIGHT;
-        _graphics.ApplyChanges();
-        
-        _gameObjects = new List<GameObject>();
-
-        base.Initialize();
-    }
-
-    protected override void LoadContent()
-    {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        _font = Content.Load<SpriteFont>("GameFont");
-        _bubbleTexture = Content.Load<Texture2D>("Bubble");
-
-        _rectTexture = new Texture2D(_graphics.GraphicsDevice, 3, 640);
-        Color[] data = new Color[3*640];
-        for (int i = 0; i < data.Length; i++)
-        {
-            data[i] = Color.White;
-        }
-        _rectTexture.SetData(data);
-
-        Reset();
-    }
-
-    protected override void Update(GameTime gameTime)
-    {
-        Singleton.Instance.CurrentKey = Keyboard.GetState();
-
-        _numObject = _gameObjects.Count;
-
-        switch (Singleton.Instance.CurrentGameState)
-        {
-            case Singleton.GameState.Playing:
-                for (int i = 0; i < _numObject; i++)
-                {
-                    if(_gameObjects[i].IsActive)
-                        _gameObjects[i].Update(gameTime, _gameObjects);
-                }
-                for (int i = 0; i < _numObject; i++)
-                {
-                    if(!_gameObjects[i].IsActive)
-                    {
-                    _gameObjects.RemoveAt(i);
-                    i--;
-                    _numObject--;
-                    }
-                }
-                break;
-            case Singleton.GameState.CheckBubbleAndCeiling:
-                CheckAndDestroyHangingBubbles();
-                CheckAndPushDownCeiling();
-                Singleton.Instance.CurrentGameState = Singleton.GameState.Playing;
-                break;
-        }
-
-        Console.WriteLine(Singleton.Instance.CurrentGameState);
-
-        Singleton.Instance.PreviousKey = Singleton.Instance.CurrentKey;
-
-        base.Update(gameTime);
-    }
-
-    protected override void Draw(GameTime gameTime)
-    {
-        GraphicsDevice.Clear(Color.Black);
-
-        _spriteBatch.Begin();
-
-        _numObject = _gameObjects.Count;
-
-        for (int i = 0; i < _numObject; i++)
-        {
-            _gameObjects[i].Draw(_spriteBatch);
-        }
-
-        //Play Border
-        _spriteBatch.Draw(_rectTexture, new Vector2(Singleton.PLAY_AREA_END_X, 0), null, Color.White, 0f, Vector2.Zero, 1, SpriteEffects.None, 0f);
-        _spriteBatch.Draw(_rectTexture, new Vector2(Singleton.PLAY_AREA_START_X, 0), null, Color.White, 0f, Vector2.Zero, 1, SpriteEffects.None, 0f);
-        
-        //Next Bubble Display
-        _spriteBatch.Draw(_bubbleTexture,new Vector2(Singleton.SCREEN_WIDTH / 8, 400),Singleton.GetBubbleColor(Singleton.Instance.NextBubble));
-
-        _spriteBatch.End();
-
-        _graphics.BeginDraw();
-
-        base.Draw(gameTime);
-    }
-
-    protected void Reset()
+    public void Initialize()
     {
         Singleton.Instance.GameBoard = new BubbleType[Singleton.BUBBLE_GRID_HEIGHT, Singleton.BUBBLE_GRID_WIDTH];
-
         Singleton.Instance.Random = new System.Random();
-
         Singleton.Instance.BubbleShotAmount = 0;
         Singleton.Instance.PlayAreaStartY = 0;
-
         Singleton.Instance.CurrentGameState = Singleton.GameState.Playing;
 
-        Texture2D cannonTexture = Content.Load<Texture2D>("Cannon");
+        _gameObjects = new List<GameObject>();
+    }
+
+    public void LoadContent(ContentManager content, GraphicsDevice graphicsDevice, SpriteBatch spriteBatch)
+    {
+        _spriteBatch = spriteBatch;
+
+        _font = content.Load<SpriteFont>("GameFont");
+        _bubbleTexture = content.Load<Texture2D>("Bubble");
+
+        _rectTexture = new Texture2D(graphicsDevice, 3, 640);
+        Color[] data = new Color[3 * 640];
+        for (int i = 0; i < data.Length; i++) data[i] = Color.White;
+        _rectTexture.SetData(data);
+
+        Texture2D cannonTexture = content.Load<Texture2D>("Cannon");
 
         _gameObjects.Clear();
         _gameObjects.Add(new Player(cannonTexture)
@@ -150,109 +57,51 @@ public class MainScene : Game
                 Speed = 0
             }
         });
-        
+
         foreach (GameObject s in _gameObjects)
         {
             s.Reset();
         }
     }
 
-    protected void CheckAndPushDownCeiling()
+    public void Update(GameTime gameTime)
     {
-        if(Singleton.Instance.BubbleShotAmount >= Singleton.CEILING_WAITING_TURN){
-            Singleton.Instance.BubbleShotAmount %= Singleton.CEILING_WAITING_TURN;
+        Singleton.Instance.CurrentKey = Keyboard.GetState();
+        int numObject = _gameObjects.Count;
 
-            Singleton.Instance.PlayAreaStartY += Singleton.BUBBLE_SIZE;
-
-            _numObject = _gameObjects.Count;
-
-            for (int i = 0; i < _numObject; i++)
-            {
-                if(_gameObjects[i].Name.Contains("Bubble"))
+        switch (Singleton.Instance.CurrentGameState)
+        {
+            case Singleton.GameState.Playing:
+                for (int i = 0; i < numObject; i++)
                 {
-                    _gameObjects[i].Position.Y += Singleton.BUBBLE_SIZE;
+                    if (_gameObjects[i].IsActive) _gameObjects[i].Update(gameTime, _gameObjects);
                 }
-            }
+                _gameObjects.RemoveAll(obj => !obj.IsActive);
+                break;
+
+            case Singleton.GameState.CheckBubbleAndCeiling:
+                // Add specific logic here
+                Singleton.Instance.CurrentGameState = Singleton.GameState.Playing;
+                break;
         }
+
+        Singleton.Instance.PreviousKey = Singleton.Instance.CurrentKey;
     }
 
-    protected void CheckAndDestroyHangingBubbles()
+    public void Draw(GameTime gameTime)
     {
-        for (int j = 1; j < Singleton.BUBBLE_GRID_HEIGHT; j++)
+        int numObject = _gameObjects.Count;
+
+        for (int i = 0; i < numObject; i++)
         {
-            for (int i = 0; i < Singleton.BUBBLE_GRID_WIDTH; i++)
-            {
-                //skip last column
-                if (j % 2 == 1 && i == Singleton.BUBBLE_GRID_WIDTH - 1)
-                    continue;
-
-                List<Vector2> AdjacentBubbles = new List<Vector2>();
-
-                CheckHighestHangingBubbles(new Vector2(i, j), AdjacentBubbles);
-
-                int highestRow = Singleton.BUBBLE_GRID_HEIGHT;
-
-                foreach (Vector2 b in AdjacentBubbles)
-                {
-                    if(b.Y < highestRow) highestRow = (int) b.Y;
-                }
-
-                if(highestRow != 0)
-                    DestroyBubbles(AdjacentBubbles);
-                    
-            }
+            _gameObjects[i].Draw(_spriteBatch);
         }
-    }
 
-    private void CheckHighestHangingBubbles(Vector2 boardCoord, List<Vector2> AdjacentBubbles)
-    {
-        if(AdjacentBubbles.Contains(boardCoord))
-            return;
+        _spriteBatch.Draw(_rectTexture, new Vector2(Singleton.PLAY_AREA_END_X, 0), Color.White);
+        _spriteBatch.Draw(_rectTexture, new Vector2(Singleton.PLAY_AREA_START_X, 0), Color.White);
 
-        int X = (int)boardCoord.X;
-        int Y = (int)boardCoord.Y;
-
-        AdjacentBubbles.Add(new Vector2(X, Y));
-
-        if(HaveBubble(X-1, Y)) CheckHighestHangingBubbles(new Vector2(X-1, Y), AdjacentBubbles);
-        if(HaveBubble(X+1, Y)) CheckHighestHangingBubbles(new Vector2(X+1, Y), AdjacentBubbles);
-        if(HaveBubble(X, Y-1)) CheckHighestHangingBubbles(new Vector2(X, Y-1), AdjacentBubbles);
-
-        bool isOddRow = (Y % 2 == 1);
-        
-        if (isOddRow)
-        {
-            if(HaveBubble( X+1, Y-1)) CheckHighestHangingBubbles(new Vector2(X+1, Y-1), AdjacentBubbles);
-        }
-        else
-        {
-            if(HaveBubble(X-1, Y-1)) CheckHighestHangingBubbles(new Vector2(X-1, Y-1), AdjacentBubbles);
-        }
-    }
-
-    protected bool HaveBubble(int x, int y)
-    {
-        if (x >= 0 && x < Singleton.BUBBLE_GRID_WIDTH && 
-            y >= 0 && y < Singleton.BUBBLE_GRID_HEIGHT)
-        {
-            return Singleton.Instance.GameBoard[y, x] != BubbleType.None;
-        }
-        return false;
-    }
-
-    protected void DestroyBubbles(List<Vector2> AdjacentBubbles)
-    {
-        for (int i = 0; i < AdjacentBubbles.Count; i++)
-        {
-            Singleton.Instance.GameBoard[(int)AdjacentBubbles[i].Y, (int)AdjacentBubbles[i].X] = BubbleType.None;
-            _numObject = _gameObjects.Count;
-            for (int j = 0; j < _numObject; j++)
-            {
-                if(_gameObjects[j] is Bubble && (_gameObjects[j] as Bubble).BoardCoord == AdjacentBubbles[i])
-                {
-                    _gameObjects[j].IsActive = false;
-                }
-            }
-        }
+        _spriteBatch.Draw(_bubbleTexture, new Vector2(Singleton.SCREEN_WIDTH / 8, 400),
+            Singleton.GetBubbleColor(Singleton.Instance.NextBubble));
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿using var game = new MidtermComGame.MainScene();
+﻿// using var game = new MidtermComGame.MainScene();
+// game.Run();
+using var game = new MidtermComGame.GameManager();
 game.Run();
