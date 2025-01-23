1ef6af6e9284a39fa66b959d005c84405ab6346a
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2025-01-23 18:26:09 +0700

    Add Temporary Starting Pattern

--- MainScene.cs
+++ MainScene.cs
@@ -181,8 +181,39 @@ public class MainScene
     protected void SetUpInitalChipsPattern()
     {
         //temp pattern
-        Singleton.Instance.GameBoard[0, 3] = ChipType.Green;
+        Singleton.Instance.GameBoard[0, 0] = ChipType.Red;
+        Singleton.Instance.GameBoard[0, 1] = ChipType.Red;
+        Singleton.Instance.GameBoard[0, 2] = ChipType.Yellow;
+        Singleton.Instance.GameBoard[0, 3] = ChipType.Yellow;
+        Singleton.Instance.GameBoard[0, 4] = ChipType.Blue;
+        Singleton.Instance.GameBoard[0, 5] = ChipType.Blue;
+        Singleton.Instance.GameBoard[0, 6] = ChipType.Green;
+        Singleton.Instance.GameBoard[0, 7] = ChipType.Green;
 
+        Singleton.Instance.GameBoard[1, 0] = ChipType.Red;
+        Singleton.Instance.GameBoard[1, 1] = ChipType.Red;
+        Singleton.Instance.GameBoard[1, 2] = ChipType.Yellow;
+        Singleton.Instance.GameBoard[1, 3] = ChipType.Yellow;
+        Singleton.Instance.GameBoard[1, 4] = ChipType.Blue;
+        Singleton.Instance.GameBoard[1, 5] = ChipType.Blue;
+        Singleton.Instance.GameBoard[1, 6] = ChipType.Green;
+
+        Singleton.Instance.GameBoard[2, 0] = ChipType.Blue;
+        Singleton.Instance.GameBoard[2, 1] = ChipType.Blue;
+        Singleton.Instance.GameBoard[2, 2] = ChipType.Green;
+        Singleton.Instance.GameBoard[2, 3] = ChipType.Green;
+        Singleton.Instance.GameBoard[2, 4] = ChipType.Red;
+        Singleton.Instance.GameBoard[2, 5] = ChipType.Red;
+        Singleton.Instance.GameBoard[2, 6] = ChipType.Yellow;
+        Singleton.Instance.GameBoard[2, 7] = ChipType.Yellow;
+
+        Singleton.Instance.GameBoard[3, 0] = ChipType.Blue;
+        Singleton.Instance.GameBoard[3, 1] = ChipType.Green;
+        Singleton.Instance.GameBoard[3, 2] = ChipType.Green;
+        Singleton.Instance.GameBoard[3, 3] = ChipType.Red;
+        Singleton.Instance.GameBoard[3, 4] = ChipType.Red;
+        Singleton.Instance.GameBoard[3, 5] = ChipType.Yellow;
+        Singleton.Instance.GameBoard[3, 6] = ChipType.Yellow;
 
         for (int j = 0; j < Singleton.CHIP_GRID_HEIGHT; j++)
         {
@@ -196,10 +227,11 @@ public class MainScene
 
     protected void AddChipToBoard(int i, int j)
     {
+        int offSetX = (j % 2 == 1) ? Singleton.CHIP_SIZE / 2 : 0;
         Chip newChip = new Chip(_chipTexture)
         {
             Name = "Chip",
-            Position = new Vector2(Singleton.PLAY_AREA_START_X + i * Singleton.CHIP_SIZE, j * Singleton.CHIP_SIZE),
+            Position = new Vector2(Singleton.PLAY_AREA_START_X + i * Singleton.CHIP_SIZE + offSetX, j * Singleton.CHIP_SIZE),
             ChipHitSound = _chipHitSound,
             ChipType = Singleton.Instance.GameBoard[j, i],
         };
@@ -253,7 +285,6 @@ public class MainScene
 
                 if(highestRow != 0)
                     DestroyChips(AdjacentChips);
-                    
             }
         }
     }
