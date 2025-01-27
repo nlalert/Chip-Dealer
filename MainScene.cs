3cb9390a5fa74321a3e2c51e742ff8b11c802976
Feen <kakfeenza@gmail.com>
2025-01-27 20:49:07 +0700

    add shitty sound adjustment

--- MainScene.cs
+++ MainScene.cs
@@ -51,7 +51,7 @@ public class MainScene
         _cannonTexture = content.Load<Texture2D>("Cannon");
         _ShopTexture = content.Load<Texture2D>("Shop");
         _GameOverTexture = content.Load<Texture2D>("GameOver1");
-        _PauseTexture = content.Load<Texture2D>("GameOver1");
+        _PauseTexture = content.Load<Texture2D>("Pause1");
 
 
         _rectTexture = new Texture2D(graphicsDevice, 3, 640);
@@ -119,6 +119,21 @@ public class MainScene
                 {
                     Singleton.Instance.CurrentGameState = Singleton.GameState.Playing;
                 }
+
+                // Adjust volume with Up/Down arrow keys
+                if (Singleton.Instance.CurrentKey.IsKeyDown(Keys.Up) && Singleton.Instance.Volume < 1.0f)
+                {
+                    Singleton.Instance.Volume += 0.01f; 
+                }
+                else if (Singleton.Instance.CurrentKey.IsKeyDown(Keys.Down) && Singleton.Instance.Volume > 0.0f)
+                {
+                    Singleton.Instance.Volume -= 0.01f; 
+                }
+                Singleton.Instance.Volume = MathHelper.Clamp(Singleton.Instance.Volume, 0.0f, 1.0f);
+
+                //TODO check this please
+                MediaPlayer.Volume = Singleton.Instance.Volume; 
+                SoundEffect.MasterVolume = Singleton.Instance.Volume;
                 break;
             case Singleton.GameState.GameOver:
                 if (MediaPlayer.State == MediaState.Playing)
@@ -194,6 +209,10 @@ public class MainScene
         if (Singleton.Instance.CurrentGameState == Singleton.GameState.Pause)
         {
             _spriteBatch.Draw(_PauseTexture, new Vector2((Singleton.SCREEN_WIDTH - _PauseTexture.Width) / 2, (Singleton.SCREEN_HEIGHT - _PauseTexture.Height) / 2), Color.White);
+            // Display the volume percentage
+            string volumeText = $"Volume: {(int)(Singleton.Instance.Volume * 100)}%";
+            Vector2 textSize = _font.MeasureString(volumeText);
+            _spriteBatch.DrawString(_font, volumeText, new Vector2((Singleton.SCREEN_WIDTH - textSize.X) / 2, Singleton.SCREEN_HEIGHT / 2), Color.White);
             return;
         }
 
