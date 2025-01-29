131b2e13dfbfcf9042c6be47a5ba767736555b35
Feen <kakfeenza@gmail.com>
2025-01-29 22:47:08 +0700

    change ShopChipStucture

--- GameObjects/ShopChip.cs
+++ GameObjects/ShopChip.cs
@@ -1,17 +1,29 @@
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
-public class ShopChip : Item
+public class ShopChip : GameObject
 {
+    public int Price;
+    public Keys BuyKey;
     public ChipType ChipType;
 
     public ShopChip(Texture2D texture) : base(texture)
     {
     }
-
-    public override void OnBuy(List<GameObject> gameObjects)
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_texture, Position, Viewport, Color.White);
+        base.Draw(spriteBatch);
+    }
+    public void OnBuy(List<GameObject> gameObjects)
     {
         Singleton.Instance.CurrentChip = ChipType;
-        base.OnBuy(gameObjects);
+    }
+    public bool IsClicked(MouseState mouseState)
+    {
+        Rectangle buttonBounds = new Rectangle((int)Position.X, (int)Position.Y, Viewport.Width, Viewport.Height);
+        return buttonBounds.Contains(mouseState.Position) && mouseState.LeftButton == ButtonState.Pressed;
     }
 }
