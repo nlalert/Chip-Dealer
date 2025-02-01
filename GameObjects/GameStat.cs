7d5a6cd99af573e42845eb690c646ccc45b50a89
Natkawin Somroob <creeper0864@gmail.com>
2025-02-01 22:54:45 +0700

    Updated Game Stat
    - Added a brief tutorial
    - Added current stage data

--- GameObjects/GameStat.cs
+++ GameObjects/GameStat.cs
@@ -45,25 +45,41 @@ class GameStat : GameObject
         spriteBatch.Draw(_texture, new Vector2(Position.X - Singleton.GetViewPortFromSpriteSheet("Score_Box0").Width/2, Position.Y + 16*2), _scoreBoxType, Color.White);
 
         fontSize = font.MeasureString(Singleton.Instance.Score.ToString());
-        spriteBatch.DrawString(font,Singleton.Instance.Score.ToString(),new Vector2(Position.X - fontSize.X/2, Position.Y + 16*2 + 8),Color.White);
+        spriteBatch.DrawString(font,Singleton.Instance.Score.ToString(),new Vector2(Position.X - fontSize.X/2, Position.Y + 16*2 + 10),Color.White);
 
-        spriteBatch.Draw(_texture, new Vector2(Position.X - (Singleton.GetViewPortFromSpriteSheet("Money_Box").Width + Singleton.GetViewPortFromSpriteSheet("Ceiling_Timer_Box").Width + 16)/2 , Position.Y + 16*5), 
+        spriteBatch.Draw(_texture, new Vector2(Position.X - (Singleton.GetViewPortFromSpriteSheet("Stage_Label").Width + Singleton.GetViewPortFromSpriteSheet("Stage_Box").Width + 16)/2 , Position.Y + 16*5 + 8), 
+        Singleton.GetViewPortFromSpriteSheet("Stage_Label"), Color.White);
+
+        spriteBatch.Draw(_texture, new Vector2(Position.X + (Singleton.GetViewPortFromSpriteSheet("Stage_Label").Width - Singleton.GetViewPortFromSpriteSheet("Stage_Box").Width + 16)/2 , Position.Y + 16*5),  
+        Singleton.GetViewPortFromSpriteSheet("Stage_Box"), Color.White);
+
+        fontSize = font.MeasureString(Singleton.Instance.Stage.ToString());
+        spriteBatch.DrawString(font,Singleton.Instance.Stage.ToString(),
+        new Vector2(Position.X + (Singleton.GetViewPortFromSpriteSheet("Stage_Label").Width - Singleton.GetViewPortFromSpriteSheet("Stage_Box").Width + 16)/2 + 16 - fontSize.X/2, Position.Y + 16*5 + 10),Color.White);
+
+        spriteBatch.Draw(_texture, new Vector2(Position.X - (Singleton.GetViewPortFromSpriteSheet("Money_Label").Width + Singleton.GetViewPortFromSpriteSheet("Ceiling_Turn_Label").Width + 16)/2 , Position.Y + 16*8), 
+        Singleton.GetViewPortFromSpriteSheet("Money_Label"), Color.White);
+
+        spriteBatch.Draw(_texture, new Vector2(Position.X - (Singleton.GetViewPortFromSpriteSheet("Money_Box").Width + Singleton.GetViewPortFromSpriteSheet("Ceiling_Turn_Box").Width + 16)/2 , Position.Y + 16*10), 
         Singleton.GetViewPortFromSpriteSheet("Money_Box"), Color.White);
 
         fontSize = font.MeasureString((Singleton.Instance.Score/10).ToString());
         spriteBatch.DrawString(font,(Singleton.Instance.Score/10).ToString(),
-        new Vector2(Position.X - (Singleton.GetViewPortFromSpriteSheet("Money_Box").Width + Singleton.GetViewPortFromSpriteSheet("Ceiling_Timer_Box").Width + 16)/2 + 16*3 - fontSize.X/2, Position.Y + 16*5 + 8),Color.White);
+        new Vector2(Position.X - (Singleton.GetViewPortFromSpriteSheet("Money_Box").Width + Singleton.GetViewPortFromSpriteSheet("Ceiling_Turn_Box").Width + 16)/2 + 16*3 - fontSize.X/2, Position.Y + 16*10 + 10),Color.White);
 
-        spriteBatch.Draw(_texture, new Vector2(Position.X + (Singleton.GetViewPortFromSpriteSheet("Money_Box").Width - Singleton.GetViewPortFromSpriteSheet("Ceiling_Timer_Box").Width + 16)/2 , Position.Y + 16*5),  
-        Singleton.GetViewPortFromSpriteSheet("Ceiling_Timer_Box"), Color.White);
+        spriteBatch.Draw(_texture, new Vector2(Position.X + (Singleton.GetViewPortFromSpriteSheet("Money_Label").Width - Singleton.GetViewPortFromSpriteSheet("Ceiling_Turn_Label").Width + 16)/2 , Position.Y + 16*8),  
+        Singleton.GetViewPortFromSpriteSheet("Ceiling_Turn_Label"), Color.White);
+
+        spriteBatch.Draw(_texture, new Vector2(Position.X + (Singleton.GetViewPortFromSpriteSheet("Money_Box").Width - Singleton.GetViewPortFromSpriteSheet("Ceiling_Turn_Box").Width + 16)/2 , Position.Y + 16*10),  
+        Singleton.GetViewPortFromSpriteSheet("Ceiling_Turn_Box"), Color.White);
 
         fontSize = font.MeasureString((Singleton.CEILING_WAITING_TURN - (Singleton.Instance.ChipShotAmount % Singleton.CEILING_WAITING_TURN)).ToString());
         spriteBatch.DrawString(font,(Singleton.CEILING_WAITING_TURN - (Singleton.Instance.ChipShotAmount % Singleton.CEILING_WAITING_TURN)).ToString(),
-        new Vector2(Position.X + (Singleton.GetViewPortFromSpriteSheet("Money_Box").Width - Singleton.GetViewPortFromSpriteSheet("Ceiling_Timer_Box").Width + 16)/2 + 16 - fontSize.X/2, Position.Y + 16*5 + 8),Color.White);
+        new Vector2(Position.X + (Singleton.GetViewPortFromSpriteSheet("Money_Box").Width - Singleton.GetViewPortFromSpriteSheet("Ceiling_Turn_Box").Width + 16)/2 + 16 - fontSize.X/2, Position.Y + 16*10 + 10),Color.White);
 
-        spriteBatch.Draw(_texture, new Vector2(Position.X - Singleton.GetViewPortFromSpriteSheet("Relic_Box").Width/2, Position.Y + 16*8), Singleton.GetViewPortFromSpriteSheet("Relic_Box"), Color.White);
+        spriteBatch.Draw(_texture, new Vector2(Position.X - Singleton.GetViewPortFromSpriteSheet("Relic_Box").Width/2, Position.Y + 16*13), Singleton.GetViewPortFromSpriteSheet("Relic_Box"), Color.White);
 
-        spriteBatch.Draw(_texture, new Vector2(Position.X  - Singleton.GetViewPortFromSpriteSheet("Next_Chip_Label").Width/2 , Position.Y + 16*18),Singleton.GetViewPortFromSpriteSheet("Next_Chip_Label"),Color.White);
-        spriteBatch.Draw(_texture, new Vector2(Position.X  - Singleton.GetViewPortFromSpriteSheet("Next_Chip_Box").Width/2 , Position.Y + 16*20),Singleton.GetViewPortFromSpriteSheet("Next_Chip_Box"),Color.White);
+        spriteBatch.Draw(_texture, new Vector2(Position.X  - Singleton.GetViewPortFromSpriteSheet("Next_Chip_Label").Width/2 , Position.Y + 16*23),Singleton.GetViewPortFromSpriteSheet("Next_Chip_Label"),Color.White);
+        spriteBatch.Draw(_texture, new Vector2(Position.X  - Singleton.GetViewPortFromSpriteSheet("Next_Chip_Box").Width/2 , Position.Y + 16*25),Singleton.GetViewPortFromSpriteSheet("Next_Chip_Box"),Color.White);
     }
 }
